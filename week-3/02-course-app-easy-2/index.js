2e772799609bea3988ad44459150843c66a34865
Arindam01gi <arindammaiti2018@gmail.com>
2024-09-26 23:30:26 +0530

    update course course-app-easy-2

--- week-3/02-course-app-easy-2/index.js
+++ week-3/02-course-app-easy-2/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken')
 
 app.use(express.json());
 
@@ -7,17 +8,73 @@ let ADMINS = [];
 let USERS = [];
 let COURSES = [];
 
+const secret_key = '33d327d6b6714add6267a1ae3fdfd748489c1636dffa4b57dbc939327186b42d'
+
+const generatejwt=(user)=>{
+  const payload = {username:user.username , password:user.password}
+  return jwt.sign(payload,secret_key,{expiresIn:'1h'})
+
+}
+
+const authenticateJwt = (req,res,next) =>{
+  const authHeaders = req.headers.authorization
+
+  if(authHeaders){
+    // const token = authHeader.split(' ')[1];
+    const token = authHeaders
+    jwt.verify(token,secret_key,(err,data)=>{
+      if(err){
+        res.status(403)
+      }
+      req.user = user 
+      next()
+    })
+  }
+}
+
+
+
 // Admin routes
 app.post('/admin/signup', (req, res) => {
   // logic to sign up admin
+  const admin = req.body
+  const adminExists = ADMINS.find((a) =>a.username === admin.username)
+  if (adminExists){
+    res.status(403).send({message:"Admin username already exists"})
+  }else{
+    ADMINS.push(admin)
+    const token = generatejwt(admin)
+    // console.log(token)
+    res.status(201).send({message:"Admin created successfully", token})
+  }
+
 });
 
-app.post('/admin/login', (req, res) => {
-  // logic to log in admin
+app.post('/admin/login',  (req, res) => {
+  const { username, password } = req.headers;
+  const admin = ADMINS.find(a => a.username === username && a.password === password);
+
+
+  
+
+  if (!admin){
+      res.status(403).send({message:"Authentication Failed"})
+  }else{
+    const token = generatejwt(admin)
+      res.send({message:"Admin logged in successfully",token})
+  }
+
 });
 
-app.post('/admin/courses', (req, res) => {
+app.post('/admin/courses', authenticateJwt ,(req, res) => {
   // logic to create a course
+  const course = req.body
+  course.id = Date.now()
+
+  COURSES.push(course)
+
+  res.send({message:"Course created successfully"})
+
 });
 
 app.put('/admin/courses/:courseId', (req, res) => {
