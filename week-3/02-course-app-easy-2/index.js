6f6877bbc3cf6c67de9a92f230392967f3e0c46b
Arindam01gi <arindammaiti2018@gmail.com>
2024-09-27 23:44:52 +0530

    update in medium

--- week-3/02-course-app-easy-2/index.js
+++ week-3/02-course-app-easy-2/index.js
@@ -21,12 +21,12 @@ const authenticateJwt = (req,res,next) =>{
 
   if(authHeaders){
     // const token = authHeader.split(' ')[1];
-    const token = authHeaders
+    const token = authHeaders;
     jwt.verify(token,secret_key,(err,data)=>{
       if(err){
         res.status(403)
       }
-      req.user = user 
+      req.user = data 
       next()
     })
   }
@@ -69,29 +69,59 @@ app.post('/admin/login',  (req, res) => {
 app.post('/admin/courses', authenticateJwt ,(req, res) => {
   // logic to create a course
   const course = req.body
-  course.id = Date.now()
+  course.id = COURSES.length +1
 
   COURSES.push(course)
 
-  res.send({message:"Course created successfully"})
+  res.send({message:"Course created successfully",courseId:course.id})
 
 });
 
-app.put('/admin/courses/:courseId', (req, res) => {
+app.put('/admin/courses/:courseId', authenticateJwt, (req, res) => {
   // logic to edit a course
+    const id = parseInt(req.params.courseId)
+
+    const courseIndex = COURSES.findIndex(c=> c.id === id)
+    if(courseIndex != -1){
+      const updatedCourse = Object.assign(courseIndex,req.body)
+      res.json({message:"Course updated successfully"})
+    }else{
+      res.status(404).send({message:"Course not found"})
+    }
+
 });
 
-app.get('/admin/courses', (req, res) => {
+app.get('/admin/courses', authenticateJwt ,(req, res) => {
   // logic to get all courses
+  res.json({courses:COURSES})
+
 });
 
 // User routes
 app.post('/users/signup', (req, res) => {
   // logic to sign up user
+ const user =  req.body
+ const userExists = USERS.find(u=> u.username === user.username)
+
+ if(userExists){
+  res.status(403).send({message:"Username already exists"})
+ }else{
+  USERS.push(user)
+  const token = generatejwt(user)
+  res.send({message:"User created successfully",token})
+ }
+
 });
 
 app.post('/users/login', (req, res) => {
   // logic to log in user
+  const user = req.body 
+  const userExists = USERS.find(u=> u.username === user.username && u.password === user.password)
+  if(userExists !=-1){
+    const token = generatejwt(user)
+    res.send({message:"User logged in successfully", token})
+  }
+
 });
 
 app.get('/users/courses', (req, res) => {
